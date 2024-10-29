1769d412eadad935f3f29d075e296b9feba47db1
Adem Mustajbasic <156593541+MAdem01@users.noreply.github.com.>
2024-10-29 10:49:23 +0100

    default setup

---
+++ CodeKickFc/src/main/java/com/codecool/codekickfc/CodeKickFcApplication.java
@@ -0,0 +1,13 @@
+package com.codecool.codekickfc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeKickFcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeKickFcApplication.class, args);
+	}
+
+}
