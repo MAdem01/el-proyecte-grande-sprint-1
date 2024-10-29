89c1ce18a8b630b3edbce566e6cc717ac902c663
Adem Mustajbasic <156593541+MAdem01@users.noreply.github.com.>
2024-10-29 18:25:34 +0100

    add basic home page & and basic css

---
+++ CodeKickFc/frontend/src/pages/homePage/HomePage.jsx
@@ -0,0 +1,10 @@
+import "./homePage.css"
+
+export default function HomePage() {
+    return (
+        <>
+        <img className="homePageImage" alt={"Missing image"} src={"https://images.pexels.com/photos/47354/the-ball-stadion-football-the-pitch-47354.jpeg?cs=srgb&dl=pexels-pixabay-47354.jpg&fm=jpg"}
+        />
+        </>
+    )
+}
