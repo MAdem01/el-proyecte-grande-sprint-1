34669d5508cc93ac05b906e00865c233a648c407
Akos Horvath <156071555+Akoss08@users.noreply.github.com>
2024-10-29 14:31:47 +0100

    service includes getAllUsers method

--- CodeKickFc/src/main/java/com/codecool/codekickfc/controller/UserDTO.java
+++ CodeKickFc/src/main/java/com/codecool/codekickfc/controller/UserDTO.java
@@ -1,4 +1,4 @@
 package com.codecool.codekickfc.controller;
 
-public record UserDTO() {
+public record UserDTO(String username, String firstName, String lastName, String email) {
 }
--- CodeKickFc/src/main/java/com/codecool/codekickfc/service/UserService.java
+++ CodeKickFc/src/main/java/com/codecool/codekickfc/service/UserService.java
@@ -1,7 +1,31 @@
 package com.codecool.codekickfc.service;
 
+import com.codecool.codekickfc.controller.UserDTO;
+import com.codecool.codekickfc.dao.User;
+import com.codecool.codekickfc.dao.UserDAO;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class UserService {
+    private final UserDAO userDAO;
+
+    public UserService(UserDAO userDAO) {
+        this.userDAO = userDAO;
+    }
+
+    public List<UserDTO> getAllUsers() {
+        List<User> users = userDAO.getAllUsers();
+        List<UserDTO> userDTOs = new ArrayList<>();
+
+        for (User user : users) {
+            UserDTO userDTO =
+                    new UserDTO(user.username(), user.firstName(), user.lastName(), user.email());
+            userDTOs.add(userDTO);
+        }
+
+        return userDTOs;
+    }
 }
