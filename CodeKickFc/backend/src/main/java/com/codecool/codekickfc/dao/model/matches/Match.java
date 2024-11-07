25e39be1c9fc0db882fdc517308ba7ad5e462d4d
Akos Horvath <156071555+Akoss08@users.noreply.github.com>
2024-11-07 17:45:53 +0100

    Create Match class as entity

--- CodeKickFc/backend/src/main/java/com/codecool/codekickfc/dao/model/matches/Match.java
+++ CodeKickFc/backend/src/main/java/com/codecool/codekickfc/dao/model/matches/Match.java
@@ -1,6 +1,79 @@
 package com.codecool.codekickfc.dao.model.matches;
 
+import com.codecool.codekickfc.dao.model.pitches.FootballPitch;
+import com.codecool.codekickfc.dao.model.users.User;
+import jakarta.persistence.*;
+
 import java.time.LocalDateTime;
 import java.util.List;
 
-public record Match(int match_id, List<Short> subscribed_players_id, double match_fee_per_players, int field_id, LocalDateTime match_date) { }
+@Entity
+public class Match {
+    @Id
+    @GeneratedValue
+    private long id;
+    @Column(nullable = false)
+    private int maxPlayers;
+    @Column(nullable = false)
+    private double matchFeePerPerson;
+    @Column(nullable = false)
+    private LocalDateTime matchDate;
+    @Column(nullable = false)
+    private String matchRules;
+    @OneToOne
+    private FootballPitch footballField;
+    @ManyToMany
+    private List<User> users;
+
+    public int getMaxPlayers() {
+        return maxPlayers;
+    }
+
+    public void setMaxPlayers(int maxPlayers) {
+        this.maxPlayers = maxPlayers;
+    }
+
+    public double getMatchFeePerPerson() {
+        return matchFeePerPerson;
+    }
+
+    public void setMatchFeePerPerson(double matchFeePerPerson) {
+        this.matchFeePerPerson = matchFeePerPerson;
+    }
+
+    public LocalDateTime getMatchDate() {
+        return matchDate;
+    }
+
+    public void setMatchDate(LocalDateTime matchDate) {
+        this.matchDate = matchDate;
+    }
+
+    public String getMatchRules() {
+        return matchRules;
+    }
+
+    public void setMatchRules(String matchRules) {
+        this.matchRules = matchRules;
+    }
+
+    public FootballPitch getFootballField() {
+        return footballField;
+    }
+
+    public void setFootballField(FootballPitch footballField) {
+        this.footballField = footballField;
+    }
+
+    public List<User> getUsers() {
+        return users;
+    }
+
+    public void addUser(User user) {
+        this.users.add(user);
+    }
+
+    public long getId() {
+        return id;
+    }
+}
