8553bc0d7d2c1dace3f0b32119f46bc34466fd5b
Akos Horvath <156071555+Akoss08@users.noreply.github.com>
2024-11-07 17:47:33 +0100

    Create FootballPitch class as an entity

--- CodeKickFc/backend/src/main/java/com/codecool/codekickfc/dao/model/pitches/FootballPitch.java
+++ CodeKickFc/backend/src/main/java/com/codecool/codekickfc/dao/model/pitches/FootballPitch.java
@@ -1,3 +1,21 @@
 package com.codecool.codekickfc.dao.model.pitches;
 
-public record FootballPitch(int Id, String name, int maxPlayers, double price, int postCode, String city, String street, int street_number) { }
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity
+public class FootballPitch {
+    @Id
+    @GeneratedValue
+    private long id;
+    @Column(unique = true, nullable = false)
+    private String pitchName;
+    @Column(nullable = false)
+    private String pitchDescription;
+    @Column(nullable = false)
+    private String pitchType;
+    @Column(unique = true, nullable = false)
+    private String address;
+}
