0b4d4dfadabda65c8406a2b0e5e7da04e04bcd4c
Akos Horvath <156071555+Akoss08@users.noreply.github.com>
2024-11-08 11:36:00 +0100

    extends UserDTO record by implementing fromUser method

--- CodeKickFc/backend/src/main/java/com/codecool/codekickfc/controller/dto/users/UserDTO.java
+++ CodeKickFc/backend/src/main/java/com/codecool/codekickfc/controller/dto/users/UserDTO.java
@@ -1,9 +1,23 @@
 package com.codecool.codekickfc.controller.dto.users;
 
-import com.codecool.codekickfc.dao.model.matches.Match;
+import com.codecool.codekickfc.controller.dto.matches.MatchDTO;
+import com.codecool.codekickfc.dao.model.users.User;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
-public record UserDTO(String username, String firstName, String lastName,
-                      String email, List<Match> matches) {
+public record UserDTO(long id, String username, String firstName, String lastName,
+                      String email, List<MatchDTO> matches) {
+    public static UserDTO fromUser(User user) {
+        return new UserDTO(
+                user.getId(),
+                user.getUsername(),
+                user.getFirstName(),
+                user.getLastName(),
+                user.getEmail(),
+                user.getMatches().stream().
+                        map(MatchDTO::fromMatch).
+                        collect(Collectors.toList())
+        );
+    }
 }
