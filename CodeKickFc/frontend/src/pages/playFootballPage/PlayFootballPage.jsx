049c1af3dc265628d6ac28673e533a1fa0cab377
Akos Horvath <156071555+Akoss08@users.noreply.github.com>
2024-11-08 19:57:55 +0100

    fetch matches from database

--- CodeKickFc/frontend/src/pages/playFootballPage/PlayFootballPage.jsx
+++ CodeKickFc/frontend/src/pages/playFootballPage/PlayFootballPage.jsx
@@ -1,17 +1,36 @@
 import "./PlayFootballPage.css"
-import {useNavigate} from "react-router-dom";
+import {useLocation, useNavigate} from "react-router-dom";
 import MatchEntry from "../../components/MatchEntry/MatchEntry.jsx";
+import {useEffect, useState} from "react";
 
-export default function PlayFootballPage(){
+export default function PlayFootballPage() {
+    const [footballMatches, setFootballMatches] = useState(null);
     const navigate = useNavigate();
-    const footballMatches = [
-        {date: "2024-01-01", location: "Archbishop, Waterloo", price: 8, maxPlayers: 16, currentPlayerCount: 8},
-        {date: "2024-01-05", location: "Stadium Park, London", price: 10, maxPlayers: 20, currentPlayerCount: 15},
-        {date: "2024-01-10", location: "Greenfield, Manchester", price: 12, maxPlayers: 18, currentPlayerCount: 10},
-        {date: "2024-01-15", location: "Victoria Ground, Leeds", price: 9, maxPlayers: 14, currentPlayerCount: 9},
-        {date: "2024-01-20", location: "Central Park, Birmingham", price: 11, maxPlayers: 22, currentPlayerCount: 20},
-        {date: "2024-01-25", location: "Sunnyfield, Liverpool", price: 7, maxPlayers: 16, currentPlayerCount: 6},
-    ];
+    const location = useLocation();
+    const queryParams = new URLSearchParams(location.search);
+    const city = queryParams.get("city");
+
+    useEffect(() => {
+        async function fetchMatches() {
+            const url = city ? `/api/matches?city=${city}` : "/api/matches";
+            try {
+                const response = await fetch(url);
+                if (!response.ok) {
+                    if (response.status === 404) {
+                        const errorData = await response.json();
+                        throw new Error(errorData.error || "Match not found");
+                    } else {
+                        throw new Error("Failed to fetch matches");
+                    }
+                }
+                const matches = await response.json();
+                console.log(matches);
+                setFootballMatches(matches);
+            } catch (error) {
+                console.error(error.message);
+                setFootballMatches({error: error.message});
+            }
+        }
 
 
     return (
