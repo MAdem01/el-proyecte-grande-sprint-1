292a5ebc8334da75fcb5228f8c9c64d01a6205ad
Akos Horvath <156071555+Akoss08@users.noreply.github.com>
2024-11-08 11:43:17 +0100

    Refactor MatchDTO record by correcting parameters and fromMatch method

--- CodeKickFc/backend/src/main/java/com/codecool/codekickfc/controller/dto/matches/MatchDTO.java
+++ CodeKickFc/backend/src/main/java/com/codecool/codekickfc/controller/dto/matches/MatchDTO.java
@@ -1,19 +1,28 @@
-//package com.codecool.codekickfc.controller.dto.matches;
-//
-//import com.codecool.codekickfc.dao.model.matches.Match;
-//
-//import java.time.LocalDateTime;
-//import java.util.List;
-//
-//public record MatchDTO(int match_id, List<Short> subscribed_players_id, double match_fee_per_players, int field_id, LocalDateTime match_date) {
-//
-//    public static MatchDTO fromMatch(Match match) {
-//        return new MatchDTO(
-//                match.match_id(),
-//                match.subscribed_players_id(),
-//                match.match_fee_per_players(),
-//                match.field_id(),
-//                match.match_date()
-//        );
-//    }
-//}
+package com.codecool.codekickfc.controller.dto.matches;
+
+import com.codecool.codekickfc.controller.dto.pitches.FootballPitchDTO;
+import com.codecool.codekickfc.controller.dto.users.SimpleUserDTO;
+import com.codecool.codekickfc.dao.model.matches.Match;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public record MatchDTO(long match_id, double match_fee_per_players, int maxPlayers,
+                       FootballPitchDTO footballPitch, LocalDateTime match_date,
+                       String matchRules, List<SimpleUserDTO> subscribedPlayers) {
+
+    public static MatchDTO fromMatch(Match match) {
+        return new MatchDTO(
+                match.getId(),
+                match.getMatchFeePerPerson(),
+                match.getMaxPlayers(),
+                FootballPitchDTO.fromFootballPitch(match.getFootballField()),
+                match.getMatchDate(),
+                match.getMatchRules(),
+                match.getUsers().stream().
+                        map(SimpleUserDTO::fromUser).
+                        collect(Collectors.toList())
+        );
+    }
+}
