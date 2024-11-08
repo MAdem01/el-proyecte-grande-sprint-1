6855380ec8a129dddcf41e44f5bf51c8b9be19bb
Akos Horvath <156071555+Akoss08@users.noreply.github.com>
2024-11-08 11:45:05 +0100

    create MatchController class by implementing getAllMatches, createMatch, updateMatch, deleteMatch, getMatchById methods, all documented

--- CodeKickFc/backend/src/main/java/com/codecool/codekickfc/controller/matches/MatchController.java
+++ CodeKickFc/backend/src/main/java/com/codecool/codekickfc/controller/matches/MatchController.java
@@ -1,34 +1,81 @@
-//package com.codecool.codekickfc.controller.matches;
-//
-//import com.codecool.codekickfc.controller.dto.matches.MatchDTO;
-//import com.codecool.codekickfc.controller.dto.matches.MatchIdDTO;
-//import com.codecool.codekickfc.controller.dto.matches.NewMatchDTO;
-//import com.codecool.codekickfc.service.matches.MatchService;
-//import org.springframework.web.bind.annotation.*;
-//
-//import java.util.List;
-//
-//@RestController
-//public class MatchController {
-//
-//    private final MatchService matchService;
-//
-//    public MatchController(MatchService matchService) {
-//        this.matchService = matchService;
-//    }
-//
-//    @GetMapping("/api/matches")
-//    public List<MatchDTO> getAllMatches() {
-//        return matchService.getAllMatches();
-//    }
-//
-//    @GetMapping("/api/matches/{matchId}")
-//    public MatchDTO getMatchById(@PathVariable int matchId) {
-//        return matchService.getMatchById(matchId);
-//    }
-//
-//    @PostMapping("/api/matches")
-//    public MatchIdDTO postMatch(@RequestBody NewMatchDTO matchDTO) {
-//        return matchService.postMatch(matchDTO);
-//    }
-//}
+package com.codecool.codekickfc.controller.matches;
+
+import com.codecool.codekickfc.controller.dto.matches.MatchDTO;
+import com.codecool.codekickfc.controller.dto.matches.MatchIdDTO;
+import com.codecool.codekickfc.controller.dto.matches.NewMatchDTO;
+import com.codecool.codekickfc.controller.dto.matches.UpdateMatchDTO;
+import com.codecool.codekickfc.service.matches.MatchService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class MatchController {
+
+    private final MatchService matchService;
+
+    public MatchController(MatchService matchService) {
+        this.matchService = matchService;
+    }
+
+    /**
+     * Extracts available information from all existing matches in the database.
+     *
+     * @return list of transformed match object that includes match's id, fee, max capacity,
+     * football field, match date, rules, subscribed players.
+     */
+    @GetMapping("/api/matches")
+    public ResponseEntity<List<MatchDTO>> getAllMatches() {
+        return ResponseEntity.ok(matchService.getAllMatches());
+    }
+
+    /**
+     * Create a new match from the provided information and saves into the database.
+     *
+     * @param newMatchDTO The request body based on the client inputs.
+     * @return ID of the newly created match.
+     */
+    @PostMapping("/api/matches")
+    public ResponseEntity<MatchIdDTO> createMatch(@RequestBody NewMatchDTO newMatchDTO) {
+        return ResponseEntity.ok(matchService.createMatch(newMatchDTO));
+    }
+
+    /**
+     * Update an existing match based on the provided information and saves the changes in the
+     * database.
+     *
+     * @param updateMatchDetails The request body based on the client inputs.
+     * @param matchId            ID of the match client wants to update.
+     * @return ID of the updated match.
+     */
+    @PutMapping("/{matchId}")
+    public ResponseEntity<MatchIdDTO> updateMatch(@PathVariable long matchId,
+                                            @RequestBody UpdateMatchDTO updateMatchDetails) {
+        return ResponseEntity.ok(matchService.updateMatch(updateMatchDetails, matchId));
+    }
+
+    /**
+     * Delete an existing match based on the provided ID and saves the changes in the
+     * database.
+     *
+     * @param matchId ID of the match client wants to delete.
+     * @return ID of the deleted match
+     */
+    @DeleteMapping("/{matchId}")
+    public ResponseEntity<MatchIdDTO> deleteMatch(@PathVariable long matchId) {
+        return ResponseEntity.ok(matchService.deleteMatch(matchId));
+    }
+
+    /**
+     * Get an existing match from the database based on the provided ID.
+     *
+     * @param matchId ID of the match client wants to get.
+     * @return Match data transfer object that includes match's id, fee, max capacity,
+     * football field, match date, rules, subscribed players.
+     */
+    @GetMapping("/api/matches/{matchId}")
+    public ResponseEntity<MatchDTO> getMatchById(@PathVariable long matchId) {
+        return ResponseEntity.ok(matchService.getMatchById(matchId));
+    }
+}
