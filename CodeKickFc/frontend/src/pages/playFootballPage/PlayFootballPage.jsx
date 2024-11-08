0ab67eac8245f3798bc5dbbd80a8bcbcd393ba1d
Akos Horvath <156071555+Akoss08@users.noreply.github.com>
2024-11-08 19:58:25 +0100

    implement formatDate method

--- CodeKickFc/frontend/src/pages/playFootballPage/PlayFootballPage.jsx
+++ CodeKickFc/frontend/src/pages/playFootballPage/PlayFootballPage.jsx
@@ -32,6 +32,19 @@ export default function PlayFootballPage() {
             }
         }
 
+        fetchMatches();
+    }, [city]);
+
+
+    function formatDate(dateString) {
+        const date = new Date(dateString);
+        const year = date.getFullYear();
+        const month = String(date.getMonth() + 1).padStart(2, '0');
+        const day = String(date.getDate()).padStart(2, '0');
+        const hours = String(date.getHours()).padStart(2, '0');
+        const minutes = String(date.getMinutes()).padStart(2, '0');
+        return `${year}-${month}-${day} ${hours}:${minutes}`;
+    }
 
     return (
         <section className="playFootballPage">
