29d3287b0ec4bb52c29bb2b7c6d64b8d773ccd0d
Akos Horvath <156071555+Akoss08@users.noreply.github.com>
2024-11-08 11:41:20 +0100

    implement MatchService class with getAllMatches, createMatch, updateMatch, deleteMatch, getMatchById methods, all documented

--- CodeKickFc/backend/src/main/java/com/codecool/codekickfc/service/matches/MatchService.java
+++ CodeKickFc/backend/src/main/java/com/codecool/codekickfc/service/matches/MatchService.java
@@ -1,33 +1,140 @@
-//package com.codecool.codekickfc.service.matches;
-//
-//import com.codecool.codekickfc.controller.dto.matches.MatchDTO;
-//import com.codecool.codekickfc.controller.dto.matches.MatchIdDTO;
-//import com.codecool.codekickfc.controller.dto.matches.NewMatchDTO;
-//import com.codecool.codekickfc.dao.matches.MatchDAO;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.stereotype.Service;
-//
-//import java.util.List;
-//
-//@Service
-//public class MatchService {
-//
-//    private final MatchDAO matchDAO;
-//
-//    @Autowired
-//    public MatchService(MatchDAO matchDAO) {
-//        this.matchDAO = matchDAO;
-//    }
-//
-//    public List<MatchDTO> getAllMatches() {
-//        return matchDAO.getAllMatches().stream().map(MatchDTO::fromMatch).toList();
-//    }
-//
-//    public MatchDTO getMatchById(int matchId) {
-//        return MatchDTO.fromMatch(matchDAO.getMatchById(matchId));
-//    }
-//
-//    public MatchIdDTO postMatch(NewMatchDTO matchDTO) {
-//        return new MatchIdDTO(matchDAO.postMatch(matchDTO));
-//    }
-//}
+package com.codecool.codekickfc.service.matches;
+
+import com.codecool.codekickfc.controller.dto.matches.MatchDTO;
+import com.codecool.codekickfc.controller.dto.matches.MatchIdDTO;
+import com.codecool.codekickfc.controller.dto.matches.NewMatchDTO;
+import com.codecool.codekickfc.controller.dto.matches.UpdateMatchDTO;
+import com.codecool.codekickfc.dao.matches.MatchRepository;
+import com.codecool.codekickfc.dao.model.matches.Match;
+import com.codecool.codekickfc.dao.model.users.User;
+import com.codecool.codekickfc.exceptions.DatabaseAccessException;
+import com.codecool.codekickfc.exceptions.MatchNotFoundException;
+import com.codecool.codekickfc.exceptions.UserNotFoundException;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class MatchService {
+
+    private final MatchRepository matchRepository;
+
+    @Autowired
+    public MatchService(MatchRepository matchRepository) {
+        this.matchRepository = matchRepository;
+    }
+
+    /**
+     * Get all information from upcoming matches through the database, and then it filters the
+     * unnecessary details by converting the objects into DTO-s.
+     *
+     * @return A list of match data transfer object that includes match's id, fee, max capacity,
+     * football field, match date, rules, subscribed players.
+     * @throws UserNotFoundException In case of no match in database.
+     */
+    public List<MatchDTO> getAllMatches() {
+        List<Match> matches = matchRepository.findUpcomingMatchesOrderByDateDesc();
+
+        if (matches.isEmpty()) {
+            throw new MatchNotFoundException();
+        }
+
+        return matches.stream().map(MatchDTO::fromMatch).collect(Collectors.toList());
+    }
+
+    /**
+     * Creates a new {@link Match} model from the provided {@link NewMatchDTO} request body,
+     * and then it filters the unnecessary details by extracting only the ID from the
+     * saved data.
+     *
+     * @param newMatchDTO The request body based on the client inputs.
+     * @return ID of the created Match model
+     * @throws DatabaseAccessException In case of connection failure.
+     */
+    public MatchIdDTO createMatch(NewMatchDTO newMatchDTO) {
+        Match newMatch = new Match(
+                newMatchDTO.maxPlayers(),
+                newMatchDTO.matchFeePerPerson(),
+                newMatchDTO.matchDate(),
+                newMatchDTO.matchRules(),
+                newMatchDTO.footballPitch()
+        );
+        try {
+            return new MatchIdDTO(matchRepository.save(newMatch).getId());
+        } catch (DataAccessException e) {
+            throw new DatabaseAccessException(e);
+        }
+    }
+
+    /**
+     * Get all information from the match found in the database by its ID, update its fields,
+     * and then it filters the unnecessary details by extracting only the ID from the saved data.
+     *
+     * @param updateMatchDetails The request body based on the client inputs.
+     * @param matchId            ID of the match client wants to update.
+     * @return ID of the updated Match model.
+     * @throws MatchNotFoundException  In case of match doesn't exist.
+     * @throws DatabaseAccessException In case of connection failure.
+     */
+    public MatchIdDTO updateMatch(UpdateMatchDTO updateMatchDetails, long matchId) {
+        Match updatedMatch = matchRepository.findById(matchId).
+                orElseThrow(MatchNotFoundException::new);
+
+        updatedMatch.setMaxPlayers(updateMatchDetails.maxPlayers());
+        updatedMatch.setMatchFeePerPerson(updateMatchDetails.matchFeePerPerson());
+        updatedMatch.setMatchDate(updateMatchDetails.matchDate());
+        updatedMatch.setMatchRules(updateMatchDetails.matchRules());
+        updatedMatch.setFootballField(updateMatchDetails.footballPitch());
+
+        try {
+            return new MatchIdDTO(matchRepository.save(updatedMatch).getId());
+        } catch (DataAccessException e) {
+            throw new DatabaseAccessException(e);
+        }
+    }
+
+    /**
+     * Get all information from the match found in the database by its ID, deletes it and also
+     * deletes itself from all associated users.
+     *
+     * @param matchId ID of the match client wants to delete.
+     * @return ID of the deleted match.
+     * @throws MatchNotFoundException   In case of match doesn't exist.
+     * @throws DatabaseAccessException In case of connection failure.
+     */
+    @Transactional
+    public MatchIdDTO deleteMatch(long matchId) {
+        Match deletableMatch = matchRepository.findById(matchId).
+                orElseThrow(MatchNotFoundException::new);
+
+        for (User user : deletableMatch.getUsers()) {
+            user.removeMatch(deletableMatch);
+        }
+
+        try {
+            matchRepository.delete(deletableMatch);
+            return new MatchIdDTO(deletableMatch.getId());
+        } catch (DataAccessException e) {
+            throw new DatabaseAccessException(e);
+        }
+    }
+
+    /**
+     * Get all information from the match found in the database by its ID, and then it filters
+     * the unnecessary details by converting the object into DTO.
+     *
+     * @param matchId ID of the match client wants to get.
+     * @return Match data transfer object that includes match's id, fee, max capacity,
+     * football field, match date, rules, subscribed players.
+     * @throws MatchNotFoundException In case of match doesn't exist.
+     */
+    public MatchDTO getMatchById(long matchId) {
+        Match match = matchRepository.findById(matchId).orElseThrow(MatchNotFoundException::new);
+
+        return MatchDTO.fromMatch(match);
+    }
+}
