d3b641ba1ec9e0942c007fda47c7ed7e26932e7d
Akos Horvath <156071555+Akoss08@users.noreply.github.com>
2024-11-08 19:55:42 +0100

    refactor controller advice to return json response

--- CodeKickFc/backend/src/main/java/com/codecool/codekickfc/controller/generalControllerAdvice/GeneralControllerAdvice.java
+++ CodeKickFc/backend/src/main/java/com/codecool/codekickfc/controller/generalControllerAdvice/GeneralControllerAdvice.java
@@ -7,41 +7,54 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @ControllerAdvice
 public class GeneralControllerAdvice {
 
     @ResponseBody
     @ExceptionHandler(DatabaseAccessException.class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    public String handleDatabaseAccessException(DatabaseAccessException e) {
-        return e.getMessage();
+    public Map<String, String> handleDatabaseAccessException(DatabaseAccessException e) {
+        Map<String, String> response = new HashMap<>();
+        response.put("error", e.getMessage());
+        return response;
     }
 
     @ResponseBody
     @ExceptionHandler(UserNotFoundException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
-    public String handleUserNotFoundException(UserNotFoundException e) {
-        return e.getMessage();
+    public Map<String, String> handleUserNotFoundException(UserNotFoundException e) {
+        Map<String, String> response = new HashMap<>();
+        response.put("error", e.getMessage());
+        return response;
     }
 
     @ResponseBody
     @ExceptionHandler(MatchNotFoundException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
-    public String handleMatchNotFoundException(MatchNotFoundException e) {
-        return e.getMessage();
+    public Map<String, String> handleMatchNotFoundException(MatchNotFoundException e) {
+        Map<String, String> response = new HashMap<>();
+        response.put("error", e.getMessage());
+        return response;
     }
 
     @ResponseBody
     @ExceptionHandler(FootballPitchNotFoundException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
-    public String handlePitchNotFoundException(FootballPitchNotFoundException e) {
-        return e.getMessage();
+    public Map<String, String> handlePitchNotFoundException(FootballPitchNotFoundException e) {
+        Map<String, String> response = new HashMap<>();
+        response.put("error", e.getMessage());
+        return response;
     }
 
     @ResponseBody
     @ExceptionHandler(AlreadySignedUpException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public String handleAlreadySignedUpException(AlreadySignedUpException e) {
-        return e.getMessage();
+    public Map<String, String> handleAlreadySignedUpException(AlreadySignedUpException e) {
+        Map<String, String> response = new HashMap<>();
+        response.put("error", e.getMessage());
+        return response;
     }
 }
