35e9280442ee2a54215cda54a03dff12d12bc13a
Adem Mustajbasic <156593541+MAdem01@users.noreply.github.com.>
2024-11-12 11:45:17 +0100

    add InputField.jsx component

---
+++ CodeKickFc/frontend/src/components/InputField/InputField.jsx
@@ -0,0 +1,6 @@
+
+export default function InputField(props) {
+    return (
+        <input className={props.className} placeholder={props.placeholder} type={props.type} value={props.value} onChange={props.onChange} />
+    )
+}
