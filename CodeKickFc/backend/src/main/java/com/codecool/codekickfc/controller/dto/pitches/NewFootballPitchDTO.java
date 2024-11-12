3c483e5a5d9c55a9e761034d563506b680be09ca
Akos Horvath <156071555+Akoss08@users.noreply.github.com>
2024-11-12 11:18:54 +0100

    refactor new pitch DTO address, separated to city, district, str name, str number, postcode

--- CodeKickFc/backend/src/main/java/com/codecool/codekickfc/controller/dto/pitches/NewFootballPitchDTO.java
+++ CodeKickFc/backend/src/main/java/com/codecool/codekickfc/controller/dto/pitches/NewFootballPitchDTO.java
@@ -1,5 +1,6 @@
 package com.codecool.codekickfc.controller.dto.pitches;
 
 public record NewFootballPitchDTO(String pitchName, String description,
-                                  String pitchType, String address) {
+                                  String pitchType, String city, String district, String postcode,
+                                  String streetName, String streetNumber) {
 }
