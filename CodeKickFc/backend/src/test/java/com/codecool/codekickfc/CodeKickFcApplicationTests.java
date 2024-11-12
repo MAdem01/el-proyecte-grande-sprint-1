8a01da54d9693c5c9c3d559fee090b4ec5c4ebdc
Akos Horvath <156071555+Akoss08@users.noreply.github.com>
2024-11-12 11:25:33 +0100

    change ddl-auto to create

--- CodeKickFc/backend/src/test/java/com/codecool/codekickfc/CodeKickFcApplicationTests.java
+++ CodeKickFc/backend/src/test/java/com/codecool/codekickfc/CodeKickFcApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class CodeKickFcApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+//	@Test
+//	void contextLoads() {
+//	}
 
 }
