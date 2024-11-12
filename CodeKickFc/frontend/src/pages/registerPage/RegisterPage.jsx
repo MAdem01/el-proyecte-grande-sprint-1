4890e104684b2366b8d3c76c90b36afbd675ac11
Adem Mustajbasic <156593541+MAdem01@users.noreply.github.com.>
2024-11-12 12:00:03 +0100

    add Login button to registerPage

--- CodeKickFc/frontend/src/pages/registerPage/RegisterPage.jsx
+++ CodeKickFc/frontend/src/pages/registerPage/RegisterPage.jsx
@@ -1,13 +1,22 @@
 import "./RegisterPage.css"
 import InputField from "../../components/InputField/InputField.jsx";
 import {useState} from "react";
+import {useNavigate} from "react-router-dom";
 
 export default function RegisterPage(){
+    const navigate = useNavigate();
     const [username, setUsername] = useState("");
     const [firstName, setFirstName] = useState("");
     const [lastName, setLastName] = useState("");
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
+    const [passwordConfirm, setPasswordConfirm] = useState("");
+
+    function handleOnLoginClick(e){
+        e.preventDefault();
+
+        navigate("/users/login");
+    }
 
     return (
         <div className="registerPageWrapper">
@@ -18,7 +27,8 @@ export default function RegisterPage(){
                     <InputField className="registerPageInputField" placeholder= "Last Name" type="text" value={lastName} onChange={(e) => setLastName(e.target.value)}/>
                     <InputField className="registerPageInputField" placeholder= "Email" type="email" value={email} onChange={(e) => setEmail(e.target.value)}/>
                     <InputField className="registerPageInputField" placeholder= "Password" type="password" value={password} onChange={(e) => setPassword(e.target.value)}/>
-                    <InputField className="registerPageInputField" placeholder= "Password confirmation" type="password" value={password} onChange={(e) => setPassword(e.target.value)}/>
+                    <InputField className="registerPageInputField" placeholder= "Password confirmation" type="password" value={passwordConfirm} onChange={(e) => setPasswordConfirm(e.target.value)}/>
+                <button className="registerPageLoginButton" onClick={handleOnLoginClick}>Login</button>
                 </form>
             </div>
         </div>
