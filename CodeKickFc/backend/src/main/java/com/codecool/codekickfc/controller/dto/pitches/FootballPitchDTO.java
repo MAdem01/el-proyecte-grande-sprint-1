558d609db5944aca54ba4453df0974a81de44048
Akos Horvath <156071555+Akoss08@users.noreply.github.com>
2024-11-12 11:20:54 +0100

    separate FootballPitch address to city, str name, str number, postcode, district

--- CodeKickFc/backend/src/main/java/com/codecool/codekickfc/controller/dto/pitches/FootballPitchDTO.java
+++ CodeKickFc/backend/src/main/java/com/codecool/codekickfc/controller/dto/pitches/FootballPitchDTO.java
@@ -3,7 +3,8 @@ package com.codecool.codekickfc.controller.dto.pitches;
 import com.codecool.codekickfc.dao.model.pitches.FootballPitch;
 
 public record FootballPitchDTO(long id, String name, String description,
-                               String pitchType, String address) {
+                               String pitchType, String city, String district, String postcode,
+                               String streetName, String streetNumber) {
 
     public static FootballPitchDTO fromFootballPitch(FootballPitch footballPitch) {
         return new FootballPitchDTO(
@@ -11,6 +12,11 @@ public record FootballPitchDTO(long id, String name, String description,
                 footballPitch.getPitchName(),
                 footballPitch.getPitchDescription(),
                 footballPitch.getPitchType(),
-                footballPitch.getAddress());
+                footballPitch.getCity(),
+                footballPitch.getDistrict(),
+                footballPitch.getPostcode(),
+                footballPitch.getStreetName(),
+                footballPitch.getStreetNumber()
+        );
     }
 }
--- CodeKickFc/backend/src/main/java/com/codecool/codekickfc/dao/model/pitches/FootballPitch.java
+++ CodeKickFc/backend/src/main/java/com/codecool/codekickfc/dao/model/pitches/FootballPitch.java
@@ -16,15 +16,28 @@ public class FootballPitch {
     private String pitchDescription;
     @Column(nullable = false)
     private String pitchType;
+    @Column(nullable = false)
+    private String city;
+    @Column(nullable = false)
+    private String district;
     @Column(unique = true, nullable = false)
-    private String address;
+    private String postcode;
+    @Column(nullable = false)
+    private String streetName;
+    @Column(nullable = false)
+    private String streetNumber;
 
     public FootballPitch(String pitchName, String pitchDescription,
-                         String pitchType, String address) {
+                         String pitchType, String city, String district, String postcode,
+                         String streetName, String streetNumber) {
         this.pitchName = pitchName;
         this.pitchDescription = pitchDescription;
         this.pitchType = pitchType;
-        this.address = address;
+        this.city = city;
+        this.district = district;
+        this.postcode = postcode;
+        this.streetName = streetName;
+        this.streetNumber = streetNumber;
     }
 
     public FootballPitch() {
@@ -58,11 +71,43 @@ public class FootballPitch {
         this.pitchType = pitchType;
     }
 
-    public String getAddress() {
-        return address;
+    public String getCity() {
+        return city;
     }
 
-    public void setAddress(String address) {
-        this.address = address;
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getDistrict() {
+        return district;
+    }
+
+    public void setDistrict(String district) {
+        this.district = district;
+    }
+
+    public String getPostcode() {
+        return postcode;
+    }
+
+    public void setPostcode(String postcode) {
+        this.postcode = postcode;
+    }
+
+    public String getStreetName() {
+        return streetName;
+    }
+
+    public void setStreetName(String streetName) {
+        this.streetName = streetName;
+    }
+
+    public String getStreetNumber() {
+        return streetNumber;
+    }
+
+    public void setStreetNumber(String streetNumber) {
+        this.streetNumber = streetNumber;
     }
 }
