984bf740043d7fcd4e2865d443c72d6212ef852a
Adem Mustajbasic <156593541+MAdem01@users.noreply.github.com.>
2024-11-12 13:25:10 +0100

    add register button & submit logic for the button

--- CodeKickFc/frontend/src/pages/registerPage/RegisterPage.jsx
+++ CodeKickFc/frontend/src/pages/registerPage/RegisterPage.jsx
@@ -11,24 +11,60 @@ export default function RegisterPage(){
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const [passwordConfirm, setPasswordConfirm] = useState("");
+    const [isPasswordValid, setIsPasswordValid] = useState(true);
 
-    function handleOnLoginClick(e){
+    function handleLoginClick(e){
         e.preventDefault();
 
         navigate("/users/login");
     }
 
+    function checkIfPasswordIsValid(){
+        if(password !== passwordConfirm || passwordConfirm === "" || password === ""){
+            setIsPasswordValid(false);
+            return false
+        }else if(password === passwordConfirm){
+            setIsPasswordValid(true);
+            return true
+        }
+    }
+
+    async function handleSubmit(e){
+        e.preventDefault();
+
+
+        if(!checkIfPasswordIsValid()) return
+
+       const userIdResponse = await fetch('/api/users', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({username: username, firstName: firstName, lastName: lastName, email: email, password: password}),
+        })
+
+        const userId = await userIdResponse.json();
+        localStorage.clear();
+        localStorage.setItem("userId", userId);
+
+
+        navigate("/");
+    }
+
+
     return (
         <div className="registerPageWrapper">
             <div className="formContainer">
-                <form className="registerForm">
+                <form className="registerForm" onSubmit={handleSubmit}>
+                {isPasswordValid ? null : <h4 className="passwordErrorMessage"> Password does not match</h4>}
                     <InputField className="registerPageInputField" placeholder= "Username" type="text" value={username} onChange={(e) => setUsername(e.target.value)}/>
                     <InputField className="registerPageInputField" placeholder= "First Name" type="text" value={firstName} onChange={(e) => setFirstName(e.target.value)}/>
                     <InputField className="registerPageInputField" placeholder= "Last Name" type="text" value={lastName} onChange={(e) => setLastName(e.target.value)}/>
                     <InputField className="registerPageInputField" placeholder= "Email" type="email" value={email} onChange={(e) => setEmail(e.target.value)}/>
                     <InputField className="registerPageInputField" placeholder= "Password" type="password" value={password} onChange={(e) => setPassword(e.target.value)}/>
                     <InputField className="registerPageInputField" placeholder= "Password confirmation" type="password" value={passwordConfirm} onChange={(e) => setPasswordConfirm(e.target.value)}/>
-                <button className="registerPageLoginButton" onClick={handleOnLoginClick}>Login</button>
+                    <button type= "submit" className="registerPageRegisterButton">Register</button>
+                    <button className="registerPageLoginButton" onClick={handleLoginClick}>Login</button>
                 </form>
             </div>
         </div>
