e49fed967502abeb416131fae4b99d5b87b90ec4
Akos Horvath <156071555+Akoss08@users.noreply.github.com>
2024-11-13 14:09:00 +0100

    add match details button to match entries page

--- CodeKickFc/frontend/src/components/MatchEntry/MatchEntry.jsx
+++ CodeKickFc/frontend/src/components/MatchEntry/MatchEntry.jsx
@@ -1,11 +1,15 @@
 import "./MatchEntry.css"
 
-export default function MatchEntry(props){
+export default function MatchEntry(props) {
     return (
         <div className="matchEntryBox">
             <h3 className="matchDate">{props.date}</h3>
             <h3 className="matchLocation">{props.city + ", " + props.district}</h3>
             <h3 className="matchPrice">{props.price + "£"}</h3>
+            <button className="matchDetails"
+                    onClick={() => props.navigate(`/matchdetails/${props.match.match_id}`,
+                        {state: {match: props.match}})}>Details
+            </button>
             <div className="matchPlayerCountBox">
                 <h3 className="currentPlayerCount">{props.currentPlayerCount}</h3>
                 <h3 className="maxPlayerCount">{"/ " + props.maxPlayers}</h3>
