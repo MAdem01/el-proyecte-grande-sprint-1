fdd8998186b8890be7b4c152cbee04468ada35f0
Adem Mustajbasic <156593541+MAdem01@users.noreply.github.com.>
2024-11-15 07:03:53 +0100

    add jpa specification executor

--- CodeKickFc/backend/src/main/java/com/codecool/codekickfc/dao/matches/MatchRepository.java
+++ CodeKickFc/backend/src/main/java/com/codecool/codekickfc/dao/matches/MatchRepository.java
@@ -4,19 +4,15 @@ import com.codecool.codekickfc.dao.model.matches.Match;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Repository
-public interface MatchRepository extends JpaRepository<Match, Long> {
-    @Query("SELECT m FROM Match m WHERE m.matchDate > CURRENT_TIMESTAMP " +
-            "ORDER BY m.matchDate ASC")
-    Page<Match> findUpcomingMatchesOrderByDateAsc(Pageable pageable);
+public interface MatchRepository extends JpaRepository<Match, Long>, JpaSpecificationExecutor<Match> {
 
-    @Query("SELECT m FROM Match m JOIN m.footballField f " +
-            "WHERE m.matchDate > CURRENT_TIMESTAMP AND f.city ILIKE %:city%" +
-            " ORDER BY m.matchDate ASC")
-    Page<Match> findUpcomingMatchesOrderByDateAscAndByCity(String city, Pageable pageable);
 }
