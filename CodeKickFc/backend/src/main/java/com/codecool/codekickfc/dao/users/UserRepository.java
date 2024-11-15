212bb225bf6fee553c31d37feb9b57d361f33821
Akos Horvath <156071555+Akoss08@users.noreply.github.com>
2024-11-15 14:03:31 +0100

    remove findUpcomingMatchesByUserId query from user repository

--- CodeKickFc/backend/src/main/java/com/codecool/codekickfc/dao/users/UserRepository.java
+++ CodeKickFc/backend/src/main/java/com/codecool/codekickfc/dao/users/UserRepository.java
@@ -1,17 +1,10 @@
 package com.codecool.codekickfc.dao.users;
 
-import com.codecool.codekickfc.dao.model.matches.Match;
 import com.codecool.codekickfc.dao.model.users.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
-
-import java.util.List;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-    @Query("SELECT m FROM Match m " +
-            "JOIN m.users u " +
-            "WHERE u.id = :userId AND m.matchDate > CURRENT_TIMESTAMP")
-    List<Match> findUpcomingMatchesByUserId(long userId);
+
 }
