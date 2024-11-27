ac34b6fa7607ba9732fb5b4f32e26b5d99e7755d
Akos Horvath <156071555+Akoss08@users.noreply.github.com>
2024-11-27 10:39:45 +0100

    implement confirmation email method in email controller class

--- CodeKickFc/backend/src/main/java/com/codecool/codekickfc/controller/email/EmailController.java
+++ CodeKickFc/backend/src/main/java/com/codecool/codekickfc/controller/email/EmailController.java
@@ -1,5 +1,6 @@
 package com.codecool.codekickfc.controller.email;
 
+import com.codecool.codekickfc.controller.dto.email.EmailConfirmationDTO;
 import com.codecool.codekickfc.controller.dto.email.EmailDTO;
 import com.codecool.codekickfc.service.email.EmailService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,6 +16,12 @@ public class EmailController {
     @PostMapping("/send")
     public String sendEmail(@RequestBody EmailDTO request) {
         emailService.sendEmail(request.subject(), request.description());
+        return "Email sent successfully!";
+    }
+
+    @PostMapping("/confirmation")
+    public String confirmationEmail(@RequestBody EmailConfirmationDTO request) {
+        emailService.sendConfirmationEmail(request);
         return "Email sent successfully!";
     }
 }
