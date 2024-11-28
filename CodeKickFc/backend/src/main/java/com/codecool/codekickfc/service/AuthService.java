52307f9474ac80f10cd2ceb2c474d0e16b0d61ba
Adem Mustajbasic <156593541+MAdem01@users.noreply.github.com.>
2024-11-28 12:54:57 +0100

    add service for getting userDetails

--- CodeKickFc/backend/src/main/java/com/codecool/codekickfc/service/AuthService.java
+++ CodeKickFc/backend/src/main/java/com/codecool/codekickfc/service/AuthService.java
@@ -4,28 +4,30 @@ import com.codecool.codekickfc.dto.users.JwtResponse;
 import com.codecool.codekickfc.dto.users.LoginRequest;
 import com.codecool.codekickfc.repository.UserRepository;
 import com.codecool.codekickfc.security.jwt.JwtUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
-
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Service
 public class AuthService {
 
     private final AuthenticationManager authenticationManager;
     private final JwtUtils jwtUtils;
-    private final UserRepository userRepository;
 
     @Autowired
     public AuthService(AuthenticationManager authenticationManager, JwtUtils jwtUtils, UserRepository userRepository) {
         this.authenticationManager = authenticationManager;
         this.jwtUtils = jwtUtils;
-        this.userRepository = userRepository;
     }
 
     public JwtResponse authenticateUser(LoginRequest loginRequest) {
@@ -33,10 +35,24 @@ public class AuthService {
                 new UsernamePasswordAuthenticationToken(loginRequest.username(), loginRequest.password())
         );
 
+
         UserDetails userDetails = (UserDetails) authentication.getPrincipal();
         String jwt = jwtUtils.generateJwtToken(authentication);
         List<String> roles =userDetails.getAuthorities().stream().map(GrantedAuthority::getAuthority).toList();
 
+
         return new JwtResponse(jwt, userDetails.getUsername(), roles);
+    }
+
+    public Map<String, Object> getUserDetails() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication != null && authentication.isAuthenticated()) {
+            UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+            Map<String, Object> userInfo = new HashMap<>();
+            userInfo.put("username", userDetails.getUsername());
+            userInfo.put("authorities", userDetails.getAuthorities());
+            return userInfo;
+        }
+        return null;
     }
 }
