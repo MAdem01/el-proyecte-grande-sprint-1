2c3d08842ba115d9e037efdaa6adf1602a5e10b5
Akos Horvath <156071555+Akoss08@users.noreply.github.com>
2024-11-28 10:33:50 +0100

    add user id to jwt response dto

--- CodeKickFc/backend/src/main/java/com/codecool/codekickfc/security/jwt/JwtUtils.java
+++ CodeKickFc/backend/src/main/java/com/codecool/codekickfc/security/jwt/JwtUtils.java
@@ -40,8 +40,12 @@ public class JwtUtils {
     }
 
     public String getUserNameFromJwtToken(String token) {
-        return Jwts.parserBuilder().setSigningKey(key()).build()
-                .parseClaimsJws(token).getBody().getSubject();
+        return Jwts.parserBuilder()
+                .setSigningKey(key())
+                .build()
+                .parseClaimsJws(token)
+                .getBody()
+                .getSubject();
     }
 
     public boolean validateJwtToken(String authToken) {
