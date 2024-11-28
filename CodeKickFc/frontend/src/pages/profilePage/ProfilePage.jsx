4c343df609c62efc699fe12dfb5e50608274ab61
Akos Horvath <156071555+Akoss08@users.noreply.github.com>
2024-11-28 10:25:09 +0100

    (fix) loads component if user data exists instead of checking and on false load an empty div

--- CodeKickFc/frontend/src/pages/profilePage/ProfilePage.jsx
+++ CodeKickFc/frontend/src/pages/profilePage/ProfilePage.jsx
@@ -29,35 +29,31 @@ export default function ProfilePage() {
 
     return (
         <section className="userPage">
-            {userData != null ?
-            (
-                <div className="userDataContainer">
-                    <div className="userNameContainer">
-                        <h2>{userData.firstName} {userData.lastName}</h2>
-                        <h3>{userData.username}</h3> 
-                    </div>
-                    <div className="userMatchesContainer">
-                        {userData.matches.map((footballMatch) => (
+            {userData &&
+                (
+                    <div className="userDataContainer">
+                        <div className="userNameContainer">
+                            <h2>{userData.firstName} {userData.lastName}</h2>
+                            <h3>{userData.username}</h3>
+                        </div>
+                        <div className="userMatchesContainer">
+                            {userData.matches.map((footballMatch) => (
                                 <MatchEntry
-                                key={footballMatch.match_id}
-                                date={formatDateDefault(footballMatch.match_date)}
-                                city={footballMatch.footballPitch.city}
-                                district={footballMatch.footballPitch.district}
-                                price={footballMatch.match_fee_per_players}
-                                currentPlayerCount={footballMatch.subscribedPlayers.length}
-                                maxPlayers={footballMatch.maxPlayers}
-                                matchId={footballMatch.match_id}
-                                navigate={navigate}
-                                match={footballMatch}
-                            />
-                        ))}
+                                    key={footballMatch.match_id}
+                                    date={formatDateDefault(footballMatch.match_date)}
+                                    city={footballMatch.footballPitch.city}
+                                    district={footballMatch.footballPitch.district}
+                                    price={footballMatch.match_fee_per_players}
+                                    currentPlayerCount={footballMatch.subscribedPlayers.length}
+                                    maxPlayers={footballMatch.maxPlayers}
+                                    matchId={footballMatch.match_id}
+                                    navigate={navigate}
+                                    match={footballMatch}
+                                />
+                            ))}
+                        </div>
                     </div>
-                </div>
-            ) : 
-            (
-                <div>
-                </div>
-            )}
+                )}
         </section>
     )
 }
