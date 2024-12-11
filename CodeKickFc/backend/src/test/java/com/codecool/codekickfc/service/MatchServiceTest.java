bb3d4df94b747ee610d9a165e61dde3db7c491c1
KocsisAGyorgy <gurikocsis31@gmail.com>
2024-12-11 10:22:26 +0100

    added setup-

--- CodeKickFc/backend/src/test/java/com/codecool/codekickfc/service/MatchServiceTest.java
+++ CodeKickFc/backend/src/test/java/com/codecool/codekickfc/service/MatchServiceTest.java
@@ -1,15 +1,21 @@
 package com.codecool.codekickfc.service;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest(properties = "spring.profiles.active=test")
 class MatchServiceTest {
 
+    @BeforeEach
+    void setUp() {
+
+
+    }
+
     @Test
     void getAllMatches() {
     }
-
 
     @Test
     void createMatch() {
