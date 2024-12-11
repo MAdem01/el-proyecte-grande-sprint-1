9a36122319763aac5b211a2e39a334ba45a413df
KocsisAGyorgy <gurikocsis31@gmail.com>
2024-12-11 10:15:20 +0100

    setup test class, no work done

---
+++ CodeKickFc/backend/src/test/java/com/codecool/codekickfc/service/MatchServiceTest.java
@@ -0,0 +1,29 @@
+package com.codecool.codekickfc.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(properties = "spring.profiles.active=test")
+class MatchServiceTest {
+
+    @Test
+    void getAllMatches() {
+    }
+
+
+    @Test
+    void createMatch() {
+    }
+
+    @Test
+    void updateMatch() {
+    }
+
+    @Test
+    void deleteMatch() {
+    }
+
+    @Test
+    void getMatchById() {
+    }
+}
